6eb7767a0d4ec7cbfd00de78119cbb3be78370f0
MLwithvaibhav <thevaibhavsharma17@gmail.com>
2025-08-09 16:12:12 +0530

    updated message

---
+++ Chapter 1 PS/Problem 1.py
@@ -0,0 +1,9 @@
+print(''' And I hope you cry for me like I cry for you
+Every night for you, take it easy on me, baby
+'Cause I tried with you, saw my life with you
+End of time with you, now we're strangers
+And I hope that you still cry for me like I cry for you
+Every night for you, I've been living with this lie
+Now I can't hide the truth, I can't hide the truth
+Girl, I'll cry for you
+Girl, I'll cry for you, ooh, yeah ''')
---
+++ Chapter 1/module.py
@@ -0,0 +1,12 @@
+from webbrowser import get
+import pyjokes
+
+print("printing jokes....")
+
+# this line prints a random joke
+joke = pyjokes.get_joke()
+print(joke)
+
+"""
+so thanks that was my program
+"""
