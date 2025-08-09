d2ef752b6fd064dba0b058e98714acde01f97514
MLwithvaibhav <thevaibhavsharma17@gmail.com>
2025-08-09 19:11:53 +0530

    Added chapter 3 part -2

---
+++ Chapter 3/01_Intro_to_strings.py
@@ -0,0 +1,10 @@
+a = 'harry' #single quoted string
+b = "Vaibhav" #double quoted strings
+c = '''Chudaikhane ke''' #triple quoted strings
+
+name = "harry2"
+nammeshort = name[0:3] #start from index 0 all the way till (excluding 3)
+print(nammeshort)
+
+character1 = name[1]
+print(character1)
