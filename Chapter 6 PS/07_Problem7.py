10913e3fce56ea4ead42f50155f53147e6da6560
MLwithvaibhav <thevaibhavsharma17@gmail.com>
2025-08-13 01:29:22 +0530

    Added chapter 6 and PS

---
+++ Chapter 6 PS/07_Problem7.py
@@ -0,0 +1,7 @@
+post = input("Enter the post : ")
+
+if("harry" in post.lower()):
+    print("Harry is in post")
+
+else:
+    print("Harry is not in the post")
---
+++ Chapter 6/01_conditional.py
@@ -0,0 +1,17 @@
+a = int(input("Enter your age : "))
+
+if(a>=18):
+    print("You can submit your vote  ")
+    print("Good for you ")
+
+
+elif(a<0):
+    print("Mat kar lala mat kar")
+
+
+elif(a==0):
+    print("Bakchodi mat kar lawde, hatt teri ma ki chut")
+
+else:
+    print("You can't submit vote ")
+    print("Ja na lawde teri ma ki chut ")
