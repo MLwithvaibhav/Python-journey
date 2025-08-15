814b5958c2ee8f28b3e99762b759df2b8c178a35
MLwithvaibhav <thevaibhavsharma17@gmail.com>
2025-08-15 22:19:15 +0530

    Added chapter 9 PS

---
+++ Chapter 9 PS/05_Problem.py
@@ -0,0 +1,10 @@
+words = ["bhosda", "randi", "madarchod" , "jai bheem" ,"saaale"]
+
+with open("file.txt", "r") as f:
+    content = f.read()
+
+for word in words:
+    content = content.replace(word, "#" * len(word))
+
+with open("file.txt", "w") as f:
+    f.write(content)
